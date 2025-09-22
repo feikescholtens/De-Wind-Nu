995a92ca0b46a8504447ddcf26cd98166052d49f
Feike Scholtens <feikescholtens@gmail.com>
2025-09-22 18:19:19 +0200

    fixed, quite some changes needed

--- fetchScripts/KNMI/fetchDataForDay.js
+++ fetchScripts/KNMI/fetchDataForDay.js
@@ -18,8 +18,7 @@ export async function fetchDataForDay_KNMI(dateParsed, databaseData, resolve, ti
 
   const KNMI_ID = databaseData.measurements.API_ID
   const [dateStartFetch, dateEndFetch] = getFetchDates(dateParsed, DSTDates)
-
-  const rawDataString = await fetch(`https://api.dataplatform.knmi.nl/edr/v1/collections/observations/locations/${KNMI_ID}?datetime=${dateStartFetch}/${dateEndFetch}&parameter-name=ff_10m_10,fx_10m_10,dd_10`, { headers: { "Authorization": process.env.KDP_EDR_KEY } })
+  const rawDataString = await fetch(`https://api.dataplatform.knmi.nl/edr/v1/collections/10-minute-in-situ-meteorological-observations/locations/${KNMI_ID}?datetime=${dateStartFetch}/${dateEndFetch}&parameter-name=ff,gff,dd`, { headers: { "Authorization": process.env.KDP_EDR_KEY } })
     .then(response => response.text()).catch((error) => catchFetchError(resolve, data, error, "KNMI")) // This handles all errors that can occur during the fetch, like timeouts or no internet connection
   try { rawData = JSON.parse(rawDataString) } catch { JSON_ParseError(rawDataString, resolve, "KNMI"); return } // If the data can't be parsed to JSON, log, resolve and return
   if (KNMI_API_error(rawData, resolve)) return // Check for error in the returned data
@@ -29,8 +28,7 @@ export async function fetchDataForDay_KNMI(dateParsed, databaseData, resolve, ti
     windGust = [],
     windDirection = []
   let measurementTimes = []
-
-  rawData.domain.axes.t.values.forEach(measurementTime => measurementTimes.push(ISO_StringToLocalHHmm(measurementTime, measurementTimes))) // Add all the times of the measurements to the measurementTimes array
+  rawData.coverages[0].domain.axes.t.values.forEach(measurementTime => measurementTimes.push(ISO_StringToLocalHHmm(measurementTime, measurementTimes))) // Add all the times of the measurements to the measurementTimes array
 
   times.forEach(timeStamp => { // Loop through all the times that are requested [00:00, 00:10, ..., 00:00_nextDay]
     if (measurementTimes.includes(timeStamp) == false) { // If the time is not in the measurementTimes array, add -999 to all the data arrays to indicate that there is no data for this time (probably this time is in the future)
@@ -43,13 +41,13 @@ export async function fetchDataForDay_KNMI(dateParsed, databaseData, resolve, ti
     // This only happens when the clock turns one hour back when timezones switch from CEST to CET. 02:00, 02:10, 02:20, 02:30, 02:40, 02:50 will 
     // already be in the data array, so look at the second value of these times in the measurementTimes array to get the right indici.
 
-    if (rawData.ranges.ff_10m_10.values[indexTime] != undefined) windSpeed.push(rawData.ranges.ff_10m_10.values[indexTime] * 1.94384449) // Convert m/s to knots
+    if (rawData.coverages[0].ranges.ff.values[indexTime] != undefined) windSpeed.push(rawData.coverages[0].ranges.ff.values[indexTime] * 1.94384449) // Convert m/s to knots
     else windSpeed.push(-999)
 
-    if (rawData.ranges.fx_10m_10.values[indexTime] != undefined) windGust.push(rawData.ranges.fx_10m_10.values[indexTime] * 1.94384449) // Convert m/s to knots
+    if (rawData.coverages[0].ranges.gff.values[indexTime] != undefined) windGust.push(rawData.coverages[0].ranges.gff.values[indexTime] * 1.94384449) // Convert m/s to knots
     else windGust.push(-999)
 
-    if (rawData.ranges.dd_10.values[indexTime] != undefined) windDirection.push(rawData.ranges.dd_10.values[indexTime])
+    if (rawData.coverages[0].ranges.dd.values[indexTime] != undefined) windDirection.push(rawData.coverages[0].ranges.dd.values[indexTime])
     else windDirection.push(-999)
   })
 
--- fetchScripts/KNMI/fetchDataForOverview.js
+++ fetchScripts/KNMI/fetchDataForOverview.js
@@ -19,7 +19,7 @@ export async function fetchDataForOverview_KNMI(locations, resolve) {
 
   const beginDate = subHours(new Date(), 1).toISOString() // Look for measurements from the last hour
 
-  const rawDataString = await fetch(`https://api.dataplatform.knmi.nl/edr/v1/collections/observations/cube?datetime=${beginDate}/..&parameter-name=ff_10m_10,fx_10m_10,dd_10`, { headers: { "Authorization": process.env.KDP_EDR_KEY } })
+  const rawDataString = await fetch(`https://api.dataplatform.knmi.nl/edr/v1/collections/10-minute-in-situ-meteorological-observations/cube?bbox=-180,-90,180,90&datetime=${beginDate}/..&parameter-name=ff,gff,dd`, { headers: { "Authorization": process.env.KDP_EDR_KEY } })
     .then(response => response.text()).catch((error) => catchFetchError(resolve, {}, error, "KNMI")) // This handles all errors that can occur during the fetch, like timeouts or no internet connection
   try { rawData = JSON.parse(rawDataString) } catch { JSON_ParseError(rawDataString, resolve, "KNMI"); return } // If the data can't be parsed to JSON, log, resolve and return
   if (KNMI_API_error(rawData, resolve)) return // KNMI API returns an error in the JSON when there is no data or there is another error (beside fetch errors like timeouts)
@@ -28,16 +28,16 @@ export async function fetchDataForOverview_KNMI(locations, resolve) {
 
   rawData.coverages.forEach(locationData => { // For each location with latest measurements
 
-    const KNMI_ID = locationData["inspiregloss:Identifier"] // Like 06257
+    const KNMI_ID = locationData["eumetnet:locationId"] // Like 0-20000-0-06316
     const applicationID = IDMatches[KNMI_ID] // Get application ID for the KNMI location
     if (applicationID == undefined) return
 
     const timeStampString = locationData.domain.axes.t.values.at(-1)
     const indexLastMeasurement = locationData.domain.axes.t.values.indexOf(timeStampString)
 
-    const windSpeed = locationData.ranges.ff_10m_10.values[indexLastMeasurement] * 1.94384449, // Convert m/s to knots
-      windGusts = locationData.ranges.fx_10m_10.values[indexLastMeasurement] * 1.94384449, // Convert m/s to knots
-      windDirection = locationData.ranges.dd_10.values[indexLastMeasurement]
+    const windSpeed = locationData.ranges.ff.values[indexLastMeasurement] * 1.94384449, // Convert m/s to knots
+      windGusts = locationData.ranges.gff.values[indexLastMeasurement] * 1.94384449, // Convert m/s to knots
+      windDirection = locationData.ranges.dd.values[indexLastMeasurement]
 
     data[applicationID] = {
       windSpeed: windSpeed,
